0169250ee83ac5b8bd57c12714aac2a786acb5da
Raphael Silvestre dos Santos <raphael.silvestre-ext@viavarejo.com.br>
2022-01-12 21:12:59 -0300

    Aula dia 05/01/2022

---
+++ README.md
@@ -0,0 +1,9 @@
+# Comando para listar os Projetos
+ - dotnet new --list
+
+## Comando para adicionar projetos a solução vazia
+``` bash
+A partir de uma solução criada digitamos o comando para adicionar a referencia ao csproj dos projetos
+    dotnet sln add src\TorneSe.ServicoNotaAlunos.Worker\TorneSe.ServicoNotaAlunos.Worker.csproj
+    dotnet sln remove src\TorneSe.ServicoNotaAluno.Worker\TorneSe.ServicoNotaAluno.Worker.csproj  
+ ```
---
+++ src/TorneSe.ServicoNotaAlunos.Worker/WorkerExemplo.cs
@@ -0,0 +1,50 @@
+using System.Net.Http;
+using System;
+using System.Threading;
+
+namespace TorneSe.ServicoNotaAlunos.Worker;
+
+public class WorkerExemplo : IHostedService, IDisposable
+{
+    private readonly ILogger<WorkerExemplo> _logger;
+    private Timer _timer;
+    private readonly HttpClient _client;
+    public WorkerExemplo(ILogger<WorkerExemplo> logger)
+    {
+        _logger = logger;
+        _client = new HttpClient();
+    }
+    public Task StartAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Iniciando serviço....");
+
+        _timer = new Timer(VerificarSite, null, TimeSpan.Zero, 
+            TimeSpan.FromSeconds(5));
+
+        return Task.CompletedTask;
+    }
+
+    private async void VerificarSite(object? state)
+    {
+        var response = await _client.GetAsync("https://www.google.com.br");
+
+        if(response.IsSuccessStatusCode)
+            _logger.LogInformation("O site está OK!");
+        else
+            _logger.LogError("O site não está legal!");
+    }
+
+    public Task StopAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Parando serviço....");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
